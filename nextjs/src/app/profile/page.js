eb96b1898f063331cfe0775b08c75502cdfddc48
Abhishek Abbi <abhishekabbi.work@gmail.com>
2024-11-17 18:39:51 +0530

    profile UI

---
+++ nextjs/src/app/profile/page.js
@@ -0,0 +1,40 @@
+'use client'
+// import { Layout, Breadcrumb, Card } from 'antd';
+
+
+
+
+export default function Profile() {
+
+    
+
+    
+
+    return (
+
+        
+        <div>
+            <div >
+        <div class="flex flex-col justify-center items-center h-[100vh] text-black">
+            <div class="relative flex flex-col items-center rounded-[20px] w-[400px] mx-auto p-4 bg-white bg-clip-border shadow-3xl shadow-shadow-500 drk:!bg-navy-800 drk:text-white drk:!shadow-none">
+                <div class="relative flex h-32 w-full justify-center rounded-xl bg-cover" >
+                    <img src="/randomass.jpg" class="absolute flex h-32 w-full justify-center rounded-xl bg-cover"/> 
+                    <div class="absolute -bottom-12 flex h-[87px] w-[87px] items-center justify-center rounded-full border-[4px] border-white bg-pink-400 drk:!border-navy-700">
+                    </div>
+                </div> 
+                <div class="mt-16 flex flex-col items-center">
+                    
+                </div> 
+                
+                                        
+                                        
+                
+            </div>  
+        </div>
+    </div>
+               
+                
+
+        </div>
+    )
+}
