7d6bc5aba4666b777e90cb5ad6db0784d8ba46a0
D-hruvil-Patel <dhruvilpatel685@gmail.com>
2024-11-18 19:17:24 +0530

    update button.jsx

--- nextjs/src/app/components/ui/button.jsx
+++ nextjs/src/app/components/ui/button.jsx
@@ -32,5 +32,17 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-);
+)
 
+const Button = React.forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
+  const Comp = asChild ? Slot : "button"
+  return (
+    (<Comp
+      className={cn(buttonVariants({ variant, size, className }))}
+      ref={ref}
+      {...props} />)
+  );
+})
+Button.displayName = "Button"
+
+export { Button, buttonVariants }
