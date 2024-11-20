4758836737a1e13f7832b33824fef83c7d8e7b38
Abhishek Abbi <abhishekabbi.work@gmail.com>
2024-11-20 12:59:16 +0530

    added add transaction functionality

--- nextjs/src/helpers/models/user.js
+++ nextjs/src/helpers/models/user.js
@@ -10,18 +10,12 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true
   },
-  password: {
-    type: String,
-    required: true
-  },
+
   createdAt: {
     type: Date,
     default: Date.now
   },
-  updatedAt: {
-    type: Date,
-    default: Date.now
-  }
+ 
 });
 
 const User = mongoose.model('User', userSchema);
