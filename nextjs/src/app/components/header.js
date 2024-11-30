161f587dbe9cc2f5959c4ce547ac7ff708da4035
Abhishek Abbi <abhishekabbi.work@gmail.com>
2024-11-30 13:08:16 +0530

    activate landing page

--- nextjs/src/app/components/header.js
+++ nextjs/src/app/components/header.js
@@ -9,15 +9,6 @@ import { useUser } from '@auth0/nextjs-auth0/client';
 import Link from 'next/link';
 
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "./ui/dropdown-menu"
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import { ChartBarIcon, MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/24/outline'
 import { Button } from './ui/button'
 import { Input } from './ui/input'
@@ -63,6 +54,8 @@ export function HeaderJs() {
     }, [pathname])
  
   useEffect(() => {
+
+    if (user)
     setIsLoggedIn(true);
     // setUserName(user.nickname);/
 
@@ -73,29 +66,26 @@ export function HeaderJs() {
     (<header className="bg-white shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
+          <Link href="/landing">
           <div className="flex items-center">
             {/* <ChartBarIcon className="h-8 w-8 text-blue-600 mr-2" /> */}
             <img src="/logo.jpeg" className="h-12 w-auto mr-2 -ml-6" />
             <span className="font-bold text-xl text-gray-900 ">INVESTALYZE</span>
           </div>
+          </Link>
           
           <div className={`flex items-center space-x-4 text-lg`}>
+
+            {isLoggedIn &&
             <Link href="/dashboard">
             <Button variant="ghost" className={`${dashboard? "text-blue-600 hover:underline": " text-gray-600 hover:text-gray-900 "} text-base `}>
               <ChartBarIcon className={`h-5 w-5 mr-2 `} />
               Dashboard
             </Button>
             </Link>
+}
             
             <StockSearchJsx setStock={setStock}/>
-            {/* <div className="relative">
-              <MagnifyingGlassIcon
-                className="h-5 w-5 text-gray-400 absolute left-3 top-1/2 transform -translate-y-1/2" />
-              <Input
-                type="search"
-                placeholder="Search stocks..."
-                className="pl-10 pr-4 py-2 border rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-blue-500" />
-            </div> */}
             
             {isLoggedIn ? (
               <Link href="/profile" >
