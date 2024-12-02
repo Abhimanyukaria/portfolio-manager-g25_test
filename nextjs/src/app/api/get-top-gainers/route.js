68453239a67ad9d70d563e60980c48fcbf1c568b
Abhishek Abbi <abhishekabbi.work@gmail.com>
2024-12-02 22:16:34 +0530

    fix top gainers

--- nextjs/src/app/api/get-top-gainers/route.js
+++ nextjs/src/app/api/get-top-gainers/route.js
@@ -4,8 +4,19 @@ import yahooFinance from 'yahoo-finance2';
 export async function GET(req, res) {
     try {
         // Fetch the daily gainers data
-        const queryOptions = { count: 5, region: 'US', lang: 'en-US' };
-        const result = await yahooFinance.dailyGainers(queryOptions);
+        const queryOptions = { count: 4, region: 'US', lang: 'en-US' };
+
+        let result = {};
+
+
+        try{
+            result = await yahooFinance.dailyGainers(queryOptions);
+        }
+        catch(err){
+            result = err.result;
+        }
+
+        console.log(result);
 
         // Extract relevant data for each stock
         const gainers = result.quotes.map(stock => ({
@@ -19,6 +30,8 @@ export async function GET(req, res) {
         // Return the array of objects
         return NextResponse.json(gainers);
     } catch (err) {
+
+
         console.error('Error fetching daily gainers:', err);
         return NextResponse.json({ error: 'Failed to fetch daily gainers' }, { status: 500 });
     }
